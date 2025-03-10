57502086bfcb3f815b349a35c4e8cde570ca1974
Théo Le Goc <theo@legoc.net>
2025-03-10 19:55:06 -0400

    tarpin magnum ça marche pas

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/Platform/GlfwApplication.h>
 
-int main(int argc, char** argv) {
-    std::cout << "hello world!" << std::endl;
-    return 0;
+using namespace Magnum;
+
+class MyApplication : public Platform::Application
+{
+public:
+    explicit MyApplication(const Arguments& arguments);
+    virtual ~MyApplication() = default;
+
+private:
+    void drawEvent() override;
+};
+
+MyApplication::MyApplication(const Arguments& arguments):
+    Platform::Application{arguments}
+{
+    // TODO: Add your initialization code here
 }
+
+void MyApplication::drawEvent()
+{
+    GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
+
+    // TODO: Add your drawing code here
+
+    swapBuffers();
+}
+
+MAGNUM_APPLICATION_MAIN(MyApplication)

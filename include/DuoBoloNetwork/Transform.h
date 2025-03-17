00b331e7fb6b86a5cfdb181af523ddec7dfde55b
Cocomineral <tintinch59@gmail.com>
2025-03-17 17:57:03 -0400

    fix uninitialized variables in transforms

--- include/DuoBoloNetwork/Transform.h
+++ include/DuoBoloNetwork/Transform.h
@@ -5,13 +5,14 @@
 #pragma once
 
 #include <raylib.h>
+#include <raymath.h>
 #include <entt/entt.hpp>
 #include <nlohmann/json_fwd.hpp>
 
 struct TransformComponent
 {
-    Vector3 position;
-    Quaternion rotation;
+    Vector3 position = {1, 1, 1};
+    Quaternion rotation = QuaternionIdentity();
     Vector3 scale = { 1, 1, 1 }; // only affects rendering !!
 
     void BinarySerialize(entt::handle handle, std::vector<uint8_t>& byteArray);
